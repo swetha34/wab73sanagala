a192c352e970ff785431067b7369dd361e3f53fd
swetha34 <ghanaswetha743@gmail.com>
2021-03-31 22:04:50 -0500

    added files

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var hens = require('./routes/hens');
+var stars = require('./routes/stars');
+var slot = require('./routes/slot');
 
 var app = express();
 
@@ -23,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/hens', hens);
+app.use('/stars', stars);
+app.use('/slot', slot);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
